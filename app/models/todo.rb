6b4e5d23c9acbe921c4fe6aa75bd2446ac2cd3c1
Yuying Chen <aaflying@gmail.com>
2017-10-13 21:57:40 +0800

    add story of create-todo

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,3 +1,10 @@
 class Todo < ApplicationRecord
+  after_initialize :set_defaults, unless: :persisted?
   validates_presence_of :description
+
+  def set_defaults
+    self.priority = 2
+    self.done = false
+    self.datetime_create = DateTime.current
+  end
 end
