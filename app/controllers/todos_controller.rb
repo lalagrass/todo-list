4a2b6091db92438e36418dd89e217113ea7dd3d3
Yuying Chen <aaflying@gmail.com>
2017-10-13 22:08:59 +0800

    add story for edit-todo

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -14,6 +14,17 @@ class TodosController < ApplicationController
     redirect_to todos_url
   end
 
+  def edit
+    @todo = Todo.find(params[:id])
+  end
+
+  def update
+    @todo = Todo.find(params[:id])
+    @todo.update_attributes(todo_params)
+
+    redirect_to todos_url
+  end
+
   private
 
   def todo_params
