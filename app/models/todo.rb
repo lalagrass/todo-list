555cf38dac1fa62cf8085b472f0860cf22cc3483
Yuying Chen <aaflying@gmail.com>
2017-10-14 21:38:12 +0800

    add radio button

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,9 +1,11 @@
 class Todo < ApplicationRecord
   after_initialize :set_defaults, unless: :persisted?
   validates_presence_of :description
+  enum priority: [:low, :medium, :high]
+
 
   def set_defaults
-    self.priority = 2
+    self.priority = 1
     self.done = false
     self.datetime_create = DateTime.current
   end
